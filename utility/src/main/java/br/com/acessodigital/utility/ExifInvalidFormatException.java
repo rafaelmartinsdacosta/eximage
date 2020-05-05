65c9c643afcda6422e030783745e1b20ffcb2d87
rafaelmartinsdacosta <rafael_martins_da_costa@outlook.com>
2020-05-05 15:11:40 -0300

    Initial commit

---
+++ utility/src/main/java/br/com/acessodigital/utility/ExifInvalidFormatException.java
@@ -0,0 +1,7 @@
+package br.com.acessodigital.utility;
+
+public class ExifInvalidFormatException extends Exception {
+    public ExifInvalidFormatException(String meg) {
+        super(meg);
+    }
+}
